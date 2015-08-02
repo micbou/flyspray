12b644a1b618e65918fadb460da91940d9c4db2b
micbou <contact@micbou.com>
2015-08-02 15:26:41 +0200

    Fix deprecated set_magic_quotes_runtime

--- adodb/adodb-xmlschema03.inc.php
+++ adodb/adodb-xmlschema03.inc.php
@@ -1407,7 +1407,9 @@ class adoSchema {
 	function adoSchema( &$db ) {
 		// Initialize the environment
 		$this->mgq = get_magic_quotes_runtime();
-		set_magic_quotes_runtime(0);
+        if (get_magic_quotes_runtime()) {
+            set_magic_quotes_runtime(0);
+        }
 		
 		$this->db = $db;
 		$this->debug = $this->db->debug;
@@ -2373,7 +2375,9 @@ class adoSchema {
 	* @deprecated adoSchema now cleans up automatically.
 	*/
 	function Destroy() {
-		set_magic_quotes_runtime( $this->mgq );
+        if (get_magic_quotes_runtime()) {
+            set_magic_quotes_runtime( $this->mgq );
+        }
 		unset( $this );
 	}
 }
